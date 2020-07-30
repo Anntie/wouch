2876f5e81807573269d8c9f3cd38b575885bb381
Andrew <andri.golovenko@vis-design.com>
2020-07-30 18:42:47 +0300

    Updated readme

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,21 +14,23 @@ class UsersController extends Controller
     public function active(Request $request): JsonResource
     {
         // Task 6.1
-        $limit = $request->input('posts_limit', 0);
+        $limit = (int)$request->input('posts_limit', 0);
 
         $users = WouchUser::active()->with([
             'posts' => fn($query) => $query->whereNull('deleted_at')
                 ->withCount('comments') // Task 6.2
-                ->orderBy('comments_count', 'desc'), // Task 6.3
-            'author' => fn($query) => $query->where('active', true),
+                ->orderBy('comments_count', 'desc') // Task 6.3
+                ->with([
+                    'author' => fn($query) => $query->where('active', true)
+                ]),
         ])->when(
-            $limit > 0 && is_int($limit),
-            fn($query) => $query->limit($limit),
+            $limit > 0, fn($query) => $query->limit($limit)
         )->get();
 
         return WouchUserResource::collection($users);
     }
 
+    // Task 7
     public function comments(Request $request, int $userId): JsonResource
     {
         $userCommentsRepository = app()->make(UserCommentsRepositoryInterface::class, [
